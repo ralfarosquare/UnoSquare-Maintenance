4a8fafc2a18cbaf913d9e0190b225773a83e5bb3
Rodrigo Alonso Alfaro Luna <rodrigo.alfaro@unosquare.com>
2021-09-23 17:01:22 -0500

    Fix

--- SDET_dotnet_maintenanceUnosquare/SDET_dotnet_maintenanceUnosquare/Program.cs
+++ SDET_dotnet_maintenanceUnosquare/SDET_dotnet_maintenanceUnosquare/Program.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 using OpenQA.Selenium.Chrome;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace UnoSquare_Maintenance
+namespace UnoSquare_Maintenance_
 {
     class Program
     {
@@ -21,6 +21,7 @@ namespace UnoSquare_Maintenance
             return driver;
         }
 
+        
         public void Click(IWebElement element)
         {
             element.Click();
@@ -33,15 +34,17 @@ namespace UnoSquare_Maintenance
 
         #region Google Locators
         By GoogleSearchBar = By.XPath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input");  
-        By GoogleSearIcon = By.XPath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[2]/div[2]/ul/li[1]/div/div[2]/div[1]");
+        By GoogleSearIcon = By.Name("btnK");
         By UnoSquareGoogleResult = By.PartialLinkText("Transformation");
+        
         #endregion
 
         #region UnoSquare Locators
-        By UnoSquareServicesMenu = By.XPath("/html/body/header/div/nav/div/ul/li[2]/a");
-        By PracticeAreas = By.XPath("/html/body/header/div/nav/div/ul/li[3]/a");
-        By ContactUs = By.XPath("/html/body/header/div/nav/div/ul/li[9]/a");
-        #endregion 
+        By UnoSquareServicesMenu = By.XPath("//*[@id='navbarSupportedContent']/ul/li[2]/a");
+        By PracticeAreas = By.XPath("//*[@id='navbarSupportedContent']/ul/li[3]/a");
+        By ContactUs = By.XPath("//*[@id='navbarSupportedContent']/ul/li[9]/a");
+        
+        #endregion
         static void Main(string[] args) 
         {
             IWebDriver Browser;
@@ -50,21 +53,45 @@ namespace UnoSquare_Maintenance
             Browser = program.SetUpDriver();
             Browser.Url = "https://www.google.com";
 
+            program.driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+
             element = Browser.FindElement(program.GoogleSearchBar);
 
             program.SendText(element, "Unosquare");
+           
+            Assert.IsTrue(element.Displayed);
+            Console.WriteLine("google search is displayed)");
 
             element = Browser.FindElement(program.GoogleSearIcon);
 
+
             program.Click(element);
+           
 
             element = Browser.FindElement(program.UnoSquareGoogleResult);
 
+            program.driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            var driver0 = program.driver.FindElement(By.XPath("//*[@id='rso']/div[1]/div/div/div[1]/div/div/div/div[1]/a/h3"));
+            Assert.IsTrue(driver0.Displayed);
+            
+            program.driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
             program.Click(element);
+            
+
+            
+            
+            
+
+            var driver1 = program.driver.FindElement(By.XPath("/html/body/header/div/nav/div/ul/li[5]/a"));
+            Assert.IsTrue(driver1.Displayed);
+            Console.WriteLine("OUR DNA is displayed correctly. It's in the menu bar");
 
             element = Browser.FindElement(program.UnoSquareServicesMenu);
 
             program.Click(element);
+            var driver2 = program.driver.FindElement(By.XPath("/html/body/header/div/nav/div/ul/li[6]/a"));
+            Assert.IsTrue(driver2.Displayed);
+            Console.WriteLine("Articles and Events is displayed.");
 
             element = Browser.FindElement(program.PracticeAreas);
 
@@ -75,11 +102,8 @@ namespace UnoSquare_Maintenance
             program.Click(element);
 
         }
-        [Test]
-        public void ListofTest()
-        {
-           Assert.IsTrue(driver.FindElement(By.Id("logo")).Displayed); ;
-        }
+       
+              
+
     }
 }
-
